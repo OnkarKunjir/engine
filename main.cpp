d26131f1fe37c665c3d4a08838716822cf67543e
OnkarKunjir <onkarkunjir8@gmail.com>
2021-06-29 11:34:38 +0530

    Minor fixes.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,7 @@
 #define __FILENAME__ "main.cpp"
 
 #include "engine2d.hpp"
-#include "render/render.hpp"
-#include "shapes/rect.hpp"
 #include "utils/log.hpp"
-
-#include <GLFW/glfw3.h>
-#include <glad/gl.h>
-#include <string>
-#include <utility>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/string_cast.hpp>
 
 int main() {
   Engine2d engine("engine", 800, 800);
